8b1e5a7db90ef4051efa42df9120ec5b4941988f
AkilBlanchard <akilblanchard@github.com>
2023-11-16 23:34:31 -0600

    placed print statement within the for loop

--- converter.py
+++ converter.py
@@ -6,22 +6,16 @@ from PIL import Image
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-print(image_folder, output_folder)
 
 
 #check if new/ exists, if not create it
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
 
-
-
 #loop through pokedex and convert images to png
 for i in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{i}')
     clean_name = os.path.splitext(i)[0]
     img.save(f'{output_folder}{clean_name}.png', 'png'  )
-print(clean_name)
 
-
-
-#save to new folder
+    print('Image Converted')
